a4379c73d18e04d3f00903061d8d8bbc60d48376
Artem Pazych <artem.y.pazych@gmail.com>
2023-07-29 11:44:12 -0700

    Add following functionality

--- api/app.py
+++ api/app.py
@@ -1,15 +1,13 @@
-import places.routes as places
 from config import config
 from flask import Flask, session
 from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(config)
-# TODO: consider replacing blueprint
-app.register_blueprint(places.main)
 
 server_session = Session(app)
 
+from businesses.routes import *
 from users.routes import *
 
 if __name__ == "__main__":
