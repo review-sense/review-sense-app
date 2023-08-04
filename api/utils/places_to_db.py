4cf5576b5315f5d56b816e9cfc19284e049cf4f2
Artem Pazych <artem.y.pazych@gmail.com>
2023-08-03 18:03:50 -0700

    Replace places db with users db, add dummy info to db

--- api/utils/places_to_db.py
+++ api/utils/places_to_db.py
@@ -6,41 +6,52 @@ from pymongo import MongoClient
 
 client = MongoClient(config.MONGO_DB_LOCAL)
 
+import bcrypt
 
-db = client.review_sense
+db = client.engage_sense
 
-places = db.places
-places.drop()
-places = db.places
+users = db.users
+users.drop()
+users = db.users
 
+password = "123"
+hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+role = "business"
+email1 = "business1@example.com"
+email2 = "business2@example.com"
 
-places.insert_one(
+users.insert_one(
     {
-      "_id": uuid.uuid4().hex,
-      "name": "As'salam Restaurant",
-      "type": "Cafe / Restaurants",
-      "description": "Description for Business 1",
-      "image": "business1.jpg",
-      "rating": 2.5,
-      "address":
-        "​783, Sheikh Zayed Road, Al Barsha 1, Hadaeq Mohammed Bin Rashid, Dubai",
-      "hours": "Open",
-      "time_created": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "_id": uuid.uuid4().hex,
+        "name": "As'salam Restaurant",
+        "type": "Cafe / Restaurants",
+        "description": "Description for Business 1",
+        "image": "business1.jpg",
+        "rating": 2.5,
+        "address": "​783, Sheikh Zayed Road, Al Barsha 1, Hadaeq Mohammed Bin Rashid, Dubai",
+        "hours": "Open",
+        "time_created": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "email": email1,
+        "password": hashed_password,
+        "role": role,
     },
 )
-places.insert_one(
-     {
-     "_id": uuid.uuid4().hex,
-      "name": "Diamond Avenue",
-      "type": "Jewerly store",
-      "description": "Description for Business 2",
-      "image": "business2.jpg",
-      "rating": 4.1,
-      "address": "​70, Sheikh Mohammed Bin Zayed Road, Mirdif, Mushraif, Dubai",
-      "hours": "Open",
-      "time_created": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+users.insert_one(
+    {
+        "_id": uuid.uuid4().hex,
+        "name": "Diamond Avenue",
+        "type": "Jewerly store",
+        "description": "Description for Business 2",
+        "image": "business2.jpg",
+        "rating": 4.1,
+        "address": "​70, Sheikh Mohammed Bin Zayed Road, Mirdif, Mushraif, Dubai",
+        "hours": "Open",
+        "time_created": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "email": email2,
+        "password": hashed_password,
+        "role": role,
     },
 )
 
-for place in places.find():
-    print(place)
+for user in users.find():
+    print(user)
--- client/src/queries/business.ts
+++ client/src/queries/business.ts
@@ -5,9 +5,8 @@ export const useGetBusinesses = (
   queryConfig?
 ): QueryObserverResult<ListResult<any>> => {
   return useQuery<ListResult<any>, Error>("businesses", async () => {
-    // const {data}= await axious
     const response = await fetch(
-      "http://127.0.0.1:8000/api/businesses/all-businesses"
+      "https://127.0.0.1:8000/api/businesses/all-businesses"
     );
     return response.json();
   });
