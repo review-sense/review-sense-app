fafe6bb399b0da34726e9fea4bbc97131f803122
Artem Pazych <artem.y.pazych@gmail.com>
2023-08-10 17:53:22 -0700

    Fix endpoint issue

--- api/app.py
+++ api/app.py
@@ -1,8 +1,9 @@
+import ssl
+
 from config import config
 from flask import Flask, session
-from flask_session import Session
 from flask_cors import CORS
-import ssl
+from flask_session import Session
 
 app = Flask(__name__)
 # Apply CORS to the entire application
@@ -17,8 +18,6 @@ from google_user.routes import *
 from users.routes import *
 
 # Configure SSL context
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain(certfile='certificates/cert.pem', keyfile='certificates/key.pem')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000, ssl_context=ssl_context)
+    app.run(debug=True, port=8000, ssl_context=config.SSL_CONTEXT)
